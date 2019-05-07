87c413d198714de5fe0b566101cdbd7917799e87
Douglas Hawkins <dougqh@gmail.com>
2019-05-07 13:15:46 +0300

    Continue to experiment using CSS to style custom elements
    Added support for padding, margin, border to polygraph-viewport.
    
    Switched to having SVG as a sibling of polygraph-viewport rather a
    child.

--- prototype/polygraph.js
+++ prototype/polygraph.js
@@ -1,3 +1,7 @@
+function debug(...parts) {
+  console.log(...parts);
+}
+
 function template(...parts) {
   const template = document.createElement('template');
   template.innerHTML = parts.join('');
@@ -31,7 +35,10 @@ class Viewport extends HTMLElement {
     console.log(viewportTemplate.content);
     
     this._svg = document.importNode(viewportTemplate.content, true).firstElementChild;
+    
     this.appendChild(this._svg);
+    this.after(this._svg);
+    this.style.display = 'none';
     
     this.updateDimensions();
   }
@@ -44,20 +51,44 @@ class Viewport extends HTMLElement {
     this.updateDimensions();
   }
   
+  get computedStyle() {
+    return window.getComputedStyle(this);
+  }
+  
   updateDimensions() {
     // DQH: This is defense against races in the initialization process.
     // This works for now, but probably warrants a cleaner more deliberate appoach later.
     // I'm just not sure what this is yet.
     if ( !this._svg ) return;
     
-    const style = window.getComputedStyle(this);
+    const style = this.computedStyle;
     if ( !style ) return;
+    
+    debug('viewport dims: ', [style.width, style.height]);
         
     const width = style.width !== 'auto' ? style.width : this.getAttribute('width');
     const height = style.height !== 'auto' ? style.height : this.getAttribute('height');
 
     this._svg.style.width = width;
     this._svg.style.height = height;
+    
+    this._svg.style.margin = style.margin;
+    this._svg.style.border = style.border;
+        
+    if ( !this._resizeObserver ) {
+      this._resizeObserver = new ResizeObserver(() => this.resizePanels());
+      this._resizeObserver.observe(this._svg);
+      
+      this.resizePanels();
+    }
+  }
+  
+  resizePanels() {
+    const style = this.computedStyle; 
+    debug('padding', style.padding);
+    
+    const svgBounds = this._svg.getBoundingClientRect();
+    debug('bounds', svgBounds);
   }
 };
 
