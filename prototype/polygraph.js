7f8fcd98e032b6ee490735c17e2c314db608cb3b
Douglas Hawkins <dougqh@gmail.com>
2019-05-07 11:55:58 +0300

    Moving layout set-up code into polygraph.js
    - Still debating the DOM structure and means of constructing the layout
    skeleton

--- prototype/polygraph.js
+++ prototype/polygraph.js
@@ -1,10 +1,36 @@
+function template(...parts) {
+  const template = document.createElement('template');
+  template.innerHTML = parts.join('');
+  return template;
+}
+
+const viewportTemplate = template(
+  '<svg class="polygraph-viewport" xmlns="http://www.w3.org/2000/svg">',
+    '<g class="content"></g>',
+          
+    '<rect class="left mask"></rect>',
+    '<rect class="right mask"></rect>',
+    '<rect class="top mask"></rect>',
+    '<rect class="bottom mask"></rect>',
+          
+    '<g class="left panel"></g>',
+    '<g class="right panel"></g>',
+    '<g class="top panel"></g>',
+    '<g class="bottom panel"></g>',
+  
+    '<g class="overlay"></g>',
+  '</svg>'
+);
+
 class Viewport extends HTMLElement {
   constructor() {
     super();
   }
   
   connectedCallback() {
-    this._svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    console.log(viewportTemplate.content);
+    
+    this._svg = document.importNode(viewportTemplate.content, true).firstElementChild;
     this.appendChild(this._svg);
     
     this.updateDimensions();
@@ -19,9 +45,14 @@ class Viewport extends HTMLElement {
   }
   
   updateDimensions() {
-    const style = window.getComputedStyle(this);
-    console.log('width', style.width, 'height', style.height);
+    // DQH: This is defense against races in the initialization process.
+    // This works for now, but probably warrants a cleaner more deliberate appoach later.
+    // I'm just not sure what this is yet.
+    if ( !this._svg ) return;
     
+    const style = window.getComputedStyle(this);
+    if ( !style ) return;
+        
     const width = style.width !== 'auto' ? style.width : this.getAttribute('width');
     const height = style.height !== 'auto' ? style.height : this.getAttribute('height');
 
