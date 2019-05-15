63d04f13d8d8f7315e24dbab2721908001dda550
Douglas Hawkins <dougqh@gmail.com>
2019-05-15 19:21:31 +0300

    Fleshing out scrolling support
    - Created a ScrollablePane helper class
       - now used to create the various panels that comprise the SVG
    viewport
    - Exposed the panels via getter-s
    - Fixed bug with scrollbar updating code

--- src/polygraph-viewport.js
+++ src/polygraph-viewport.js
@@ -23,17 +23,7 @@ const viewportTemplate = (() => {
   const html = [
     '<div>',
     '<svg xmlns="http://www.w3.org/2000/svg" class="polygraph viewport" style="width: 100%; height: 100%;">',
-      '<g class="underlay"></g>',
-      '<g class="content-container">',
-      '  <clipPath><rect></rect></clipPath>',
-      '  <g class="content"></g>',
-      '</g>',
-      '<g class="overlay"></g>',
-  
-      '<g class="left panel"></g>',
-      '<g class="right panel"></g>',
-      '<g class="top panel"></g>',
-      '<g class="bottom panel"></g>',
+    /* panels are filled-in via ScrollablePanel */
     '</svg>',
     '</div>'
   ];
@@ -69,6 +59,88 @@ function importScrollbars() {
   return import('./polygraph-scrollbars.js');
 }
 
+const clippedTemplate = (() => {
+  const svg = [
+    '<g class="panel-container" xmlns="http://www.w3.org/2000/svg">',
+    '  <g class="scrollable">',
+    '    <clipPath><rect></rect></clipPath>',
+    '    <g></g>',
+    '  </g>',  
+    '</g>'
+  ];
+  
+  const template = document.createElement('template');
+  template.innerHTML = svg.join('');
+  return template;
+})();
+
+const unclippedTemplate = (() => {
+  const svg = [
+    '<g class="panel-container" xmlns="http://www.w3.org/2000/svg">',
+    '  <g class="scrollable">',
+    '    <g></g>',
+    '  </g>',  
+    '</g>'
+  ];
+  
+  const template = document.createElement('template');
+  template.innerHTML = svg.join('');
+  return template;
+})();
+
+class ScrollablePanel {
+  constructor(opts) {
+    const clipped = opts.hasOwnProperty('clipped' ) ? opts.clipped : true;
+    
+    const template = clipped ? clippedTemplate : unclippedTemplate;
+    this._container = document.importNode(template.content, true).firstElementChild;
+    
+    this._scrollable = this._container.querySelector('.scrollable');
+    this._content = this._scrollable.querySelector('g');
+    
+    if ( clipped ) {
+      const clipPathId = genId();
+      
+      const clipPath = this._scrollable.querySelector('clipPath');
+      clipPath.id = clipPathId;
+      this._content.setAttribute('clip-path', `url(#${clipPathId})`);
+      
+      this._clippingRect = clipPath.querySelector('rect');
+    }
+  }
+  
+  set position([x, y]) {
+    this._container.setAttribute('transform', `translate(${x} ${y})`);
+  }  
+  
+  set dimensions({width, height}) {
+    if ( this._clippingRect ) {
+      this._clippingRect.setAttribute('width', width);
+      this._clippingRect.setAttribute('height', height);
+    }
+  }
+  
+  set scroll([xPos, yPos]) {
+    this._scrollable.setAttribute('transform', `translate(${xPos} ${yPos})`);
+  }
+  
+  get element() {
+    return this._container;
+  }
+  
+  get content() {
+    return this._content;
+  }
+  
+  get classList() {
+    return this._content.classList;
+  }
+  
+  get style() {
+    return this._content.style;
+  }
+}
+
 export class Viewport {
   static get DEFAULT_INNER_MARGINS() {
     return {
@@ -83,12 +155,37 @@ export class Viewport {
     this._div = document.importNode(viewportTemplate.content, true).firstElementChild;
     this._svg = this._div.querySelector('svg');
     
-    const contentContainer = this._svg.querySelector('.content-container');
-    const clipPathId = genId();
-    contentContainer.querySelector('clipPath').id = clipPathId;
-    contentContainer.querySelector('.content').setAttribute('clip-path', `url(#${clipPathId})`);
+    // TODO: Put the panels into a document fragment first???
+    this._underlay = new ScrollablePanel({clipped: false});
+    this._underlay.classList.add('underlay');
+    this._svg.append(this._underlay.element);
     
-    this._contentDims = { width: 0, height: 0};
+    this._content = new ScrollablePanel({clipped: true});
+    this._content.classList.add('content');
+    this._svg.append(this._content.element);
+    
+    this._top = new ScrollablePanel({clipped: true});
+    this._top.classList.add('top', 'panel');
+    this._svg.append(this._top.element);
+    
+    this._right = new ScrollablePanel({clipped: true});
+    this._right.classList.add('right', 'panel');
+    this._svg.append(this._right.element);
+    
+    this._bottom = new ScrollablePanel({clipped: true});
+    this._bottom.classList.add('bottom', 'panel');
+    this._svg.append(this._bottom.element);
+    
+    this._left = new ScrollablePanel({clipped: true});
+    this._left.classList.add('left', 'panel');
+    this._svg.append(this._left.element);
+    
+    this._overlay = new ScrollablePanel({clipped: false});
+    this._overlay.classList.add('overlay');
+    this._svg.append(this._overlay.element);
+    
+    
+    this._contentDims = {width: 0, height: 0};
     
     this._innerMargins = Viewport.DEFAULT_INNER_MARGINS;
     this._priorBounds = { width: -1, height: -1 };
@@ -99,6 +196,34 @@ export class Viewport {
     this._attachedScrollbars = false;
     
     importScrollbars().then((scrollbarsModule) => this._attachScrollbars(scrollbarsModule));
+  }
+  
+  get underlay() {
+    return this._underlay.content;
+  }
+  
+  get content() {
+    return this._content.content;
+  }
+  
+  get overlay() {
+    return this._overlay.content;
+  }
+  
+  get top() {
+    return this._top.content;
+  }
+  
+  get right() {
+    return this._right.content;
+  }
+  
+  get bottom() {
+    return this._bottom.content;
+  }
+  
+  get left() {
+    return this._left.content;
   }
   
   updateStyle(style) {
@@ -114,9 +239,9 @@ export class Viewport {
     if ( style.overflowY ) this.overflowY = style.overflowY;
   }
   
-  set visibleDimensions(dimsObj) {
-    this.visibleWidth = dimsObj.width;
-    this.visibleHeight = dimsObj.height;
+  set visibleDimensions({width, height}) {
+    this.visibleWidth = width;
+    this.visibleHeight = height;
   }
   
   set visibleWidth(width) {
@@ -139,26 +264,27 @@ export class Viewport {
     return this._div.style.height;
   }
   
-  set contentDimensions(dimsPxObj) {
-    this._contentDims = dimsPxObj;
+  set contentDimensions({width, height}) {
+    this._contentDims.width = width;
+    this._contentDims.height = height;
     
-    this.updateScrollbars();
+    this._updateScrollbars();
   }
   
   set contentWidth(width) {
     this._contentDims.width = width;
     
-    this.updateScrollbars();
+    this.refresh();
   }
   
   set contentHeight(height) {
     this._contentDims.height = height;
     
-    this.updateScrollbars();
+    this.refresh();
   }
   
   set innerMargins(marginObj) {
-    const defaults = DEFAULT_MARGINS();
+    const defaults = Viewport.DEFAULT_INNER_MARGINS;
     
     this._innerMargins = {
       top: marginObj.top || defaults.top,
@@ -261,43 +387,45 @@ export class Viewport {
       height: svgBounds.height - marginPxs.top - marginPxs.bottom
     };
     
+    // TODO: Add some padding for scrollbars???
+
     this._horizScrollbar.visibleSize = visibleContentDims.width;
-    this._vertScrollbar.contentSize = visibleContentDims.height;
+    this._horizScrollbar.contentSize = this._contentDims.width;
     
+    this._vertScrollbar.visibleSize = visibleContentDims.height;
+    this._vertScrollbar.contentSize = this._contentDims.height;
+    
+    this._underlay.position = [marginPxs.left, marginPxs.top];
+    this._underlay.dimensions = visibleContentDims;  // not clipped
+    
+    this._content.position = [marginPxs.left, marginPxs.top];
+    this._content.dimensions = visibleContentDims;
+    
+    this._overlay.position = [marginPxs.left, marginPxs.top + visibleContentDims.height];
+    this._overlay.dimensions = visibleContentDims;  // not clipped
+    
+    this._top.position = [marginPxs.left, 0];
+    this._top.dimensions = {width: visibleContentDims.width, height: marginPxs.top};
+    
+    this._right.position = [marginPxs.left + visibleContentDims.width, marginPxs.top];
+    this._right.dimensions = {width: marginPxs.right, height: visibleContentDims.height};
+    
+    this._bottom.position = [marginPxs.left, marginPxs.top + visibleContentDims.height];
+    this._bottom.dimensions = {width: visibleContentDims.width, height: marginPxs.bottom};
+    
+    this._left.position = [0, marginPxs.top];
+    this._left.dimensions = {width: marginPxs.left, height: visibleContentDims.height};
+    
+    if ( this._attachedScrollbars ) {
+      this._horizScrollbar.element.style.top = (marginPxs.top + visibleContentDims.height) + 'px';
+      this._horizScrollbar.element.style.left = marginPxs.left + 'px';
+    
+      this._vertScrollbar.element.style.top = marginPxs.top + 'px';
+      this._vertScrollbar.element.style.left = (marginPxs.left + visibleContentDims.width) + 'px';
+    }
 
-    const underlay = this._svg.querySelector('.underlay');
-    underlay.setAttribute('transform', translate(marginPxs.left, marginPxs.top));
-    
-    const content = this._svg.querySelector('.content-container');
-    content.setAttribute('transform', translate(marginPxs.left, marginPxs.top));
-    
-    const clippingRect = content.querySelector('clipPath rect');
-    clippingRect.setAttribute('width', contentDims.width);
-    clippingRect.setAttribute('height', contentDims.height);
-    
-    const overlay = this._svg.querySelector('.overlay');
-    overlay.setAttribute('transform', translate(marginPxs.left, marginPxs.top));
-    
-    const topPanel = this._svg.querySelector('.top.panel');
-    topPanel.setAttribute('transform', translate(marginPxs.left, 0));
-    
-    const rightPanel = this._svg.querySelector('.right.panel');
-    rightPanel.setAttribute('transform', translate(svgBounds.width - marginPxs.right, marginPxs.top));
-
-    const bottomPanel = this._svg.querySelector('.bottom.panel');
-    bottomPanel.setAttribute('transform', translate(marginPxs.left, svgBounds.height - marginPxs.bottom));
-    
-    const leftPanel = this._svg.querySelector('.left.panel');
-    leftPanel.setAttribute('transform', translate(0, marginPxs.top));
-
-    this._horizScrollbar.element.style.top = (marginPxs.top + contentDims.height) + 'px';
-    this._horizScrollbar.element.style.left = marginPxs.left + 'px';
-    
-    this._vertScrollbar.element.style.top = marginPxs.top + 'px';
-    this._vertScrollbar.element.style.left = (marginPxs.left + contentDims.width) + 'px';
-    
-    this._refreshPending = false;
     this._priorBounds = svgBounds;
+    this._refreshPending = false;
   }
   
   _attachScrollbars(scrollbarsModule) {
@@ -317,9 +445,35 @@ export class Viewport {
     
     this._vertScrollbar.element.style.position = 'absolute';
     
-    // call adjust panels to place scrollbars properly
-    this._refresh();
+    this._horizScrollbar.addListener(() => this._horizScrollHandler());
+    this._vertScrollbar.addListener(() => this._vertScrollHandler());
     
+    // attaching is done -- refresh to place properly
     this._attachedScrollbars = true;
+    this._refresh();
+  }
+  
+  _horizScrollHandler() {
+    const xPos = this._horizScrollbar.position;
+    const yPos = this._vertScrollbar.position;
+    
+    this._underlay.scroll = [xPos, yPos];
+    this._content.scroll = [xPos, yPos];
+    this._overlay.scroll = [xPos, yPos];
+    
+    this._top.scroll = [xPos, 0];
+    this._bottom.scroll = [xPos, 0];
+  }
+  
+  _vertScrollHandler() {
+    const xPos = this._horizScrollbar.position;
+    const yPos = this._vertScrollbar.position;
+    
+    this._underlay.scroll = [xPos, yPos];
+    this._content.scroll = [xPos, yPos];
+    this._overlay.scroll = [xPos, yPos];
+    
+    this._right.scroll = [0, yPos];
+    this._left.scroll = [0, yPos];
   }
 }
